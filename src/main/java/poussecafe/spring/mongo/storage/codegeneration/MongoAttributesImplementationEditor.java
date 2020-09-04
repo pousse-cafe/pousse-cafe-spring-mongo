91953f490fdf58c663ff5db308fab15c844858d2
Gerard Dethier <g.dethier@gmail.com>
2020-09-04 08:58:11 +0200

    feat: update attributes implementation only if necessary.

--- src/main/java/poussecafe/spring/mongo/storage/codegeneration/MongoAttributesImplementationEditor.java
+++ src/main/java/poussecafe/spring/mongo/storage/codegeneration/MongoAttributesImplementationEditor.java
@@ -10,19 +10,22 @@ import static java.util.Objects.requireNonNull;
 public class MongoAttributesImplementationEditor {
 
     public void edit() {
-        var versionTypeName = new Name("org.springframework.data.annotation.Version");
-        compilationUnitEditor.addImport(versionTypeName);
-        var idTypeName = new Name("org.springframework.data.annotation.Id");
-        compilationUnitEditor.addImport(idTypeName);
-
         var typeEditor = compilationUnitEditor.typeDeclaration();
 
-        var identifierField = typeEditor.field(AggregateAttributesImplementationEditor.IDENTIFIER_FIELD_NAME).get(0);
-        identifierField.modifiers().markerAnnotation(idTypeName);
+        var idAnnotationTypeName = new Name("org.springframework.data.annotation.Id");
+        if(!compilationUnitEditor.hasImport(idAnnotationTypeName.toString())) {
+            compilationUnitEditor.addImport(idAnnotationTypeName);
+            var identifierField = typeEditor.field(AggregateAttributesImplementationEditor.IDENTIFIER_FIELD_NAME).get(0);
+            identifierField.modifiers().markerAnnotation(idAnnotationTypeName);
+        }
 
-        var versionField = typeEditor.field(AggregateAttributesImplementationEditor.VERSION_FIELD_NAME).get(0);
-        versionField.modifiers().markerAnnotation(versionTypeName);
-        versionField.modifiers().removeAnnotations(SuppressWarnings.class);
+        var versionAnnotationTypeName = new Name("org.springframework.data.annotation.Version");
+        if(!compilationUnitEditor.hasImport(versionAnnotationTypeName.toString())) {
+            compilationUnitEditor.addImport(versionAnnotationTypeName);
+            var versionField = typeEditor.field(AggregateAttributesImplementationEditor.VERSION_FIELD_NAME).get(0);
+            versionField.modifiers().markerAnnotation(versionAnnotationTypeName);
+            versionField.modifiers().removeAnnotations(SuppressWarnings.class);
+        }
 
         compilationUnitEditor.flush();
     }
